907a45c4bda33e15319b001927c2ea92f4529727
aishubsk <aishwaryakannappan@gmail.com>
2025-04-28 09:04:14 +0530

    gittask.class

---
+++ src/main/java/aishwarya/gittask.java
@@ -0,0 +1,45 @@
+package aishwarya;
+
+public class gittask {
+public static void main(String[]args) {
+
+	String s="aishwarya";
+	String rev="";
+	for(int i = s.length()-1;i>=0;i--) {
+	char c= s.charAt(i);
+	rev =rev+c;
+	}
+
+	String s1="aishwarya";
+	int a =s1.length();
+	int d=0;
+
+	for(int i=s1.length();i>=10;i++) {
+	d=s1.charAt(i);
+
+	}
+	System.out.println(rev);
+	System.out.println("length of character"+a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
